41c5e3fadeaf3397dc010447b1f1913db7c8c783
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-09-21 13:11:27 +0600

    configure database, create UserEntity, Roles, login page, Security Configuration, MainController

---
+++ src/main/java/com/example/entity/UserEntity.java
@@ -0,0 +1,21 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name="userEntity")
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private ERole role = ERole.USER_ROLE;
+}
